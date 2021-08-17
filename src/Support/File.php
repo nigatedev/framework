d80358a5e672ef466bd6afcea458c36b08dda0d8
Abass Ben Cheik <cheikabassben@gmail.com>
2021-08-17 21:33:25 +0000

    Feat: add support for strings and filesystem

---
+++ src/Support/File.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ */
+
+namespace Nigatedev\Support;
+
+/**
+ * Support and helper for files system
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+ class File {
+   
+   /**
+    * Check whether file exist or not
+    * 
+    * @param string $fileName
+    * @return true|false
+    */
+    public static function isFile(string $fileName): bool
+    {
+      return file_exists($fileName);
+    }
+   
+   /**
+    * Check whether directory exist or not
+    * 
+    * @param string $dirName
+    * @return true|false
+    */
+    public static function isDir(string $dirName): bool
+    {
+      return is_dir($dirName);
+    }
+ }
---
+++ src/Support/Str.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ */
+
+namespace Nigatedev\Support;
+
+/**
+ * Support and helper for strings
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+ class Str {
+   
+ }
